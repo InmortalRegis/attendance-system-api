a4990be829ef0069b9ff5489f9c74c321b5886d9
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-10-18 17:43:48 -0500

    feat: mysql database

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,22 @@
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
 
-const config: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: 'db.sqlite',
+// const config: SqliteConnectionOptions = {
+//   type: 'sqlite',
+//   database: 'db.sqlite',
+//   entities: ['dist/src/**/*.entity.js'],
+//   synchronize: true,
+// };
+
+const config: MysqlConnectionOptions = {
+  host: 'behmd64f4mjub0xyd41w-mysql.services.clever-cloud.com',
+  port: 3306,
+  username: 'ud4pdadi8q7iagth',
+  password: 'UYkPfL0tP2pz3p8UQFel',
+  database: 'behmd64f4mjub0xyd41w',
   entities: ['dist/src/**/*.entity.js'],
   synchronize: true,
+  type: 'mysql',
 };
 
 export default config;
